0de39db4d2070cf181d78edcbaa2ada8519d06be
Anuj Patel <anujp@drivewithdarby.com>
2020-05-22 00:26:01 +0530

    added javascript for validation

---
+++ script/index.js
@@ -0,0 +1,18 @@
+function validateform(){
+  var fname = document.getElementById('fname');
+  var lname = document.getElementById('lname');
+  var ph = document.getElementById('phNumber');
+
+  if (fname == null || fname == "") {
+    alert("Name cant be blank");
+    return false;
+  }
+  if (lname == null || lname == "") {
+    alert("Name cant be blank");
+    return false;
+  }
+  if (phNumber.length != 10) {
+    alert("check your phone number length");
+    return false;
+  }
+}
